98505109d27762ca1bdace7e8ada0a4c9a285c34
PraneethKulukuri26 <praneethkulukuri@gmail.com>
2024-10-25 19:56:15 +0530

    Added again

---
+++ Controller/Canteen/itemController.js
@@ -0,0 +1,229 @@
+  const { measureMemory } = require("vm");
+const db=require("../../Config/mysql_DB");
+  const { checkIsAdmin } = require("./helper");
+  const fs=require('fs');
+  const path=require('path');
+
+  async function CanteengetItems(req,res){
+    try{
+      const CanteenId=req.payload.CanteenId;
+      ///console.log(CanteenID);
+
+      if(!req.payload.role==='admin'){
+        return res.json({code:-1,message:'Invalid Token.'});
+      }
+
+      const query='select * from FoodItem where CanteenId= ?';
+      const conn=await db.getConnection();
+
+      await conn.query(query,[CanteenId]).then(result=>{
+        conn.release();
+        result=result[0];
+
+        //console.log(result);
+
+        return res.json({
+          code:1,
+          message:"Items fetched Successfully",
+          data:result,
+        })
+
+      }).catch((err)=>{
+        conn.release();
+        console.log("itemController->getItems err: "+err.message);
+        return res.json({
+          code:0,
+          message:"Unable to fetch data."
+        });
+      });
+    }catch(err){
+      console.log("Failed to get items: "+err.message);
+      return res.json({code:-1,message:"Failed to get items"});
+    }
+  }
+
+
+  async function deleteItem(req,res) { 
+
+    try{
+      const itemId=req.query.id;
+      const CanteenId=req.payload.CanteenId;
+      //console.log(CanteenId);
+
+      const conn=await db.getConnection();
+      const query='delete from FoodItem where FoodItemId=? and CanteenId=?';
+
+      await conn.query(query,[itemId,CanteenId]).then((result)=>{
+        conn.release();
+
+        if(result[0].affectedRows>0){
+          return res.json({
+            code:1,
+            message:'Item removed.'
+          });
+        }else{
+          return res.json({
+            code:0,
+            message:'Item not removed.'
+          })
+        }
+      }).catch(err=>{
+        conn.release();
+        console.log(err.message);
+        return res.json({
+          code:0,
+          message:'Unable to delete.'
+        })
+      });
+    }catch(err){
+      console.log(err.message);
+      return res.json({
+        code:-1,
+        message:'Something wrong'
+      })
+    }
+  }
+
+  async function updateItem(req,res) {
+    try{
+      const itemId=req.query.id;
+      const CanteenId=req.payload.CanteenId;
+      const {Description,Price,img,Category,AvailableFrom,AvailableTo,availability,Quantity}=req.body;
+
+      if(!Description || !Category || isNaN(Price) || isNaN(Quantity)){
+        return res.json({
+          code:0,message:'Invalid data.'
+        });
+      }
+
+
+
+      const query='update FoodItem set Description = ? , Price = ? , img = ? , Category = ? , AvailableFrom = ? , AvailableTo = ? , availability = ? , Quantity = ? where itemId = ? and CanteenId = ?';
+      const conn=await db.getConnection();
+      await conn.query(query,[Description,Price,img,Category,AvailableFrom,AvailableTo,availability,Quantity,itemId,CanteenId])
+      .then(result=>{
+        conn.release();
+
+        if(!(result[0].affectedRows>0)){
+          return res.json({code:0,message:'Item Not Updated'});
+        }
+
+        return res.json({code:1,message:'Items Updated'});
+
+      }).catch(err=>{
+        conn.release();
+        console.log(err.message);
+        return res.json({code:0,message:'Failed to update'});
+      });
+
+    }catch(err){
+      console.log(err.message);
+      return res.json({
+        code:-1,
+        message:'Failed to update data.'
+      })
+    }
+  }
+
+  async function addItem(req,res) {
+
+    try{
+
+      const CanteenId=req.payload.CanteenId;
+      let images,jsonData;
+      try{
+        images=req.files.images;
+
+        if (!Array.isArray(images)) {
+          images=[images]; 
+        }
+
+        if(!images || images.length==0){
+          throw new Error("no images provided.");
+        }
+
+        for(let i=0;i<images.length;i++){
+          if(!(images[i].mimetype=='image/png' || images[i].mimetype=='image/jpeg')){
+            throw new Error("file format not accesspted.");
+          }
+        }
+
+      }catch(err){
+        console.log(err);
+        return res.json({code:0,message:err.message});
+      }
+
+      try{
+        jsonData=JSON.parse(req.body.json);
+      }catch(err){
+        console.log(err.message);
+        return res.json({code:0,message:err.message});
+      }
+
+      let { FoodItemName, Description = "", Price, Category = "veg", AvailableFrom = "9:00", AvailableTo = "17:30", Quantity = 30, availability = true } = jsonData;
+
+      // if(!Description) Description="";
+      // if(!img) img="";
+      // if(!Category) Category="veg";
+      // if(!AvailableFrom) AvailableFrom="9:00";
+      // if(!AvailableTo) AvailableTo="17:30";
+      // if(!Quantity) Quantity=30;
+      // if(availability==null) availability=true;
+      
+      if(!FoodItemName || !Price || isNaN(Price)){
+        return res.json({
+          code:0,
+          message:'Invalid Data'
+        })
+      }
+
+      const conn=await db.getConnection();
+      const query='insert into FoodItem (canteenId,FoodItemName,Description,Price,Category,AvailableFrom,AvailableTo,Quantity,availability) values(?,?,?,?,?,?,?,?,?)';
+
+      await conn.query(query,[CanteenId,FoodItemName,Description,Price,Category,AvailableFrom,AvailableTo,Quantity,availability])
+      .then(async result=>{
+        conn.release();
+
+        if(result[0].affectedRows>0){
+          
+          const dir = path.join(__dirname,'../../public/images/canteens/'+CanteenId+'/foodImages/'+result[0].insertId+'/');
+          if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });  // Create directory recursively
+          }
+
+          for(let i=0;i<images.length;i++){
+            await images[i].mv('public/images/canteens/'+CanteenId+'/foodImages/'+result[0].insertId+'/'+images[i].name);
+          }
+
+          return res.status(200).json({
+            code:1,message:'Item added.'
+          });
+        }else{
+          return res.status(200).json({
+            code:0,message:'Item not added.'
+          });
+        }
+
+      }).catch(err=>{
+        conn.release();
+        console.log(err.message);
+        return res.json({
+          code:0,
+          message:'Data not added.'
+        })
+      });
+      
+    }catch(err){
+      console.log(err);
+      return res.json({code:-1,message:'Not able to add Item.'});
+    }
+    
+  }
+
+
+  module.exports={
+    CanteengetItems,
+    deleteItem,
+    updateItem,
+    addItem
+  }
---
+++ server.js
@@ -0,0 +1,106 @@
+const express=require("express");
+const helmet = require("helmet");
+const dotenv=require("dotenv");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const compression = require("compression");
+const fileUpload = require("express-fileupload");
+const fs=require("fs");
+const path=require('path');
+
+dotenv.config();
+
+const app=express();
+
+//Middle ware
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(fileUpload());
+
+
+const verifyToken=require("./MiddleWare/verifyUserToken");
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+
+
+//admin-login
+const CanteenAuthRouter=require('./router/Canteen/authRounter')
+app.use('/api/Canteen/auth',CanteenAuthRouter);
+
+
+//admin-items
+const CanteenItemRouter=require("./router/Canteen/itemRouter")
+app.use('/api/Canteen/item',verifyToken,CanteenItemRouter);
+
+
+
+//User-auth
+const UserAuthRouter=require('./router/User/authRouter');
+app.use('/api/User/auth',UserAuthRouter);
+
+//User-items
+const UserItemRouer=require('./router/User/itemRouter');
+app.use('/api/User/item',verifyToken,UserAuthRouter);
+
+//explore
+const ExploreRouter=require('./router/explore/exploreRouter');
+app.use('/api/explore',ExploreRouter);
+
+
+
+
+app.get("/test",(req,res)=>{
+    res.json({code:17 , message: 'Test Api' , data: [
+          {
+            name: 'Praneeth',
+            id: '2300090274',
+            list: [9 , 9 , 9]
+          }
+      ]})
+});
+
+
+app.post('/test/files/',async (req,res)=>{
+  try{
+    const file=req.files.img;
+  //  if(file.length==0){
+  //   throw new 
+  //  }
+  console.log(file);
+  throw new Error("Failed tree");
+  }catch(err){
+    return res.json({code:0,message:err.message});
+  }
+
+  // const dir = path.join(__dirname, 'public/images/tree/7878/');
+  //   if (!fs.existsSync(dir)) {
+  //     fs.mkdirSync(dir, { recursive: true });  // Create directory recursively
+  //   }
+
+  //  await file[0].mv('public/images/'+file[0].name);
+
+   //image url: http://localhost:5000/images/img1.jpeg
+
+  // for(let i=0;i<file.length;i++){
+  //   await file[i].mv('public/images/tree/'+file[i].name);
+  // }
+  return res.json({code:1});
+});
+
+
+
+app.post('/test/postData',async(req,res)=>{
+  obj=JSON.parse(req.body.json);
+  //console.log(obj);
+  return res.json({code:1});
+  
+});
